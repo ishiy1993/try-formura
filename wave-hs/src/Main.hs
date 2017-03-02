e6d73c1c02cf3af092b4aa520ce7cbb7a4bdccfd
ishiy <y.ishihara.1993@gmail.com>
2017-03-02 15:05:45 +0900

    Refactoring

--- wave-hs/src/Main.hs
+++ wave-hs/src/Main.hs
@@ -1,19 +1,30 @@
 module Main where
 
+import Control.Monad (unless)
 import Data.List (intersperse)
 import Data.Vector (Vector, generate, (!))
 import qualified Data.Vector as V
+import System.Exit (die)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  let xs0 = buildInitialState 30
-      resFTCS = take 10 $ iterate (ftcs 0.5) xs0
-  writeFile "data/ftcs-05.dat" $ format resFTCS
+  as <- getArgs
+  unless (length as == 2) $ die "Need two arguments"
+  let [scheme, nu] = as
+      output = "data/" ++ scheme ++ "-" ++ nu ++ ".dat"
+      xs0 = buildInitialState 30
+      res = take 10 $ iterate (step scheme $ read nu) xs0
+  writeFile output $ format res
 
 type State = Vector (Int, Double)
 
 buildInitialState :: Int -> State
 buildInitialState x0 = generate 100 (\n -> if n < x0 then (n,1) else (n,0))
+
+step :: String -> Double -> State -> State
+step "ftcs" = ftcs
+step _ = error "No scheme"
 
 ftcs :: Double -> State -> State
 ftcs nu xs = V.map (\(i,u) -> (i, u - nu*(uR i - uL i)/2)) xs
