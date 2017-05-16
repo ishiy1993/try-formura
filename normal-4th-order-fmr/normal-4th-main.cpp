9d05ca0ccd3faedf0760bc3f4a5d21e8711eaf23
ishiy <y.ishihara.1993@gmail.com>
2017-05-16 12:13:08 +0900

    Update to calculate the precision

--- normal-4th-order-fmr/normal-4th-main.cpp
+++ normal-4th-order-fmr/normal-4th-main.cpp
@@ -5,22 +5,21 @@
 
 int mpi_my_rank;
 int T_MAX = 101;
-int T_MONITOR = 10;
+int T_MONITOR = 100;
 
-double dx = _dx_;
-double dt = _dt_;
 double u0 = 0.1;
 double p0 = 1.0;
 
 double dens(double x, double t) {
-    double x0 = 50.0*dx;
-    double a = 10.0*dx;
+    double x0 = 50.0;
+    double a = 30.0;
     return 1.0 + exp(-pow((t*u0 - (x - x0))/a*a,2));
 }
 
-void init(Formura_Navigator &navi) {
-    for(int ix = navi.lower_x + navi.offset_x; ix < navi.upper_x - navi.offset_x; ++ix) {
-        b[ix] = dens(ix*dx,0);
+void init(double dx, Formura_Navigator &navi) {
+    for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
+        double x = (ix+navi.offset_x)*dx;
+        b[ix] = dens(x,0);
         u[ix] = u0;
         p[ix] = p0;
     }
@@ -32,20 +31,23 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_my_rank);
     Formura_Init(&navi, MPI_COMM_WORLD);
 
-    init(navi);
+    double dx = 100.0/NX;
+    double dt = 0.1*dx;
+    init(dx, navi);
+
+    printf("NX = %d\n", NX);
+    printf("dx = %f\n", dx);
 
     while(navi.time_step < T_MAX) {
         if(navi.time_step % T_MONITOR == 0) {
-            printf("t = %d\n", navi.time_step);
+            printf("it = %d\n", navi.time_step);
             char fn[256];
-            sprintf(fn, "data/%f-%f-%04d.dat", dx, dt, navi.time_step);
+            sprintf(fn, "data/%d-%d.dat", NX, navi.time_step);
             FILE *fp = fopen(fn, "w");
-            for(int x = navi.lower_x; x < navi.upper_x; ++x) {
+            for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
                 double t = navi.time_step * dt;
-                double db = fabs(b[x] - dens(x*dx,t));
-                double du = fabs(u[x] - u0);
-                double dp = fabs(p[x] - p0);
-                fprintf(fp, "%d %f %f %f %f %f %f %f %f %f\n", x, b[x], u[x], p[x], dens(x*dx,t), u0, p0, db, du, dp);
+                double x = (ix + navi.offset_x)*dx;
+                fprintf(fp, "%f %f %f %f %f %f %f\n", x, b[ix], u[ix], p[ix], dens(x,t), u0, p0);
             }
             fclose(fp);
         }
