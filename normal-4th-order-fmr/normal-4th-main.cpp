82df4f72aea5e83e4ff53b5adcec5d095d21c8a6
ishiy <y.ishihara.1993@gmail.com>
2017-05-16 12:31:24 +0900

    Add total_mass()

--- normal-4th-order-fmr/normal-4th-main.cpp
+++ normal-4th-order-fmr/normal-4th-main.cpp
@@ -16,6 +16,20 @@ double dens(double x, double t) {
     return 1.0 + exp(-pow((t*u0 - (x - x0))/a*a,2));
 }
 
+double total_mass() {
+    int n = 10000;
+    double dx = 100.0/n;
+    double tm = 0;
+    for(int i=0; i<n; i++) {
+        double xL = i*dx;
+        double xR = (i+1)*dx;
+        double dL = 1/dens(xL,0);
+        double dR = 1/dens(xR,0);
+        tm += dx*(dL + dR)/2;
+    }
+    return tm;
+}
+
 void init(double dx, Formura_Navigator &navi) {
     for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
         double x = (ix+navi.offset_x)*dx;
@@ -38,17 +52,21 @@ int main(int argc, char **argv) {
     printf("NX = %d\n", NX);
     printf("dx = %f\n", dx);
 
+    double tm = total_mass();
     while(navi.time_step < T_MAX) {
         if(navi.time_step % T_MONITOR == 0) {
             printf("it = %d\n", navi.time_step);
             char fn[256];
             sprintf(fn, "data/%d-%d.dat", NX, navi.time_step);
             FILE *fp = fopen(fn, "w");
+            double sum_dens = 0;
             for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
                 double t = navi.time_step * dt;
                 double x = (ix + navi.offset_x)*dx;
                 fprintf(fp, "%f %f %f %f %f %f %f\n", x, b[ix], u[ix], p[ix], dens(x,t), u0, p0);
+                sum_dens += 1/b[ix];
             }
+            printf("%f %f %f %f\n", dx, tm, sum_dens*dx, fabs(tm - sum_dens*dx));
             fclose(fp);
         }
         Formura_Forward(&navi);
