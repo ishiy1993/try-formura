e5d0da79ce1659224f038c599e7624fcf8e26fb9
ishiy <y.ishihara.1993@gmail.com>
2017-05-19 11:26:44 +0900

    Add sin wave

---
+++ normal-4th-order-fmr/sin.c
@@ -0,0 +1,22 @@
+#include <math.h>
+
+int T_MAX = 1001;
+int T_MONITOR = 1000;
+
+double velc(double x, double t) {
+    double u0 = 0.1;
+    return u0;
+}
+
+double pres(double x, double t) {
+    double p0 = 1.0;
+    return p0;
+}
+
+double dens(double x, double t) {
+    double B0 = 1.0;
+    double B1 = 0.5;
+    double k = 1.0;
+    return B0 + B1*sin(-k*(x-velc(x,t)*t))
+}
+
---
+++ normal-4th-order-fmr/sin.h
@@ -0,0 +1,14 @@
+#ifndef SIN_H
+#define SIN_H
+
+#define problem "sin"
+
+extern int T_MAX;
+extern int T_MONITOR;
+
+double velc(double x, double t);
+double pres(double x, double t);
+double dens(double x, double t);
+
+#endif
+
