b9ea3f597de5219a0a86f27c47d6f36d132acb01
ishiy <y.ishihara.1993@gmail.com>
2017-08-30 11:20:54 +0900

    Update inital shock interpolation to fifth order

--- sode1-fmr/shocktube1/sode1-1-main.cpp
+++ sode1-fmr/shocktube1/sode1-1-main.cpp
@@ -21,21 +21,21 @@ double f(double x, double l, double r) {
     } else if (x >= x0 + d && x <= 2*x0 - d) {
         return r;
     } else if (x > x0 - d && x < x0 + d) {
-        return (l - r)*pow((x - x0)/d,3)/4.0 - 3.0*(l - r)*(x-x0)/d/4.0 + (l + r)/2.0;
+        return (l - r)*pow((x-x0)/d,5)/4.0 + (r - l)*pow((x-x0)/d,3)/4.0 + (r - l)*(x-x0)/d/2.0 + (r + l)/2.0;
     } else if (x < d) {
-        return (r - l)*pow(x/d,3)/4.0 - 3.0*(r - l)*x/d/4.0 + (r + l)/2.0;
+        return (r - l)*pow(x/d,5)/4.0 + (l - r)*pow(x/d,3)/4.0 + (l - r)*x/d/2.0 + (l + r)/2.0;
     } else {
-        return (r - l)*pow((x - 2*x0)/d,3)/4.0 - 3.0*(r - l)*(x-2*x0)/d/4.0 + (r + l)/2.0;
+        return (r - l)*pow((x-2*x0)/d,5)/4.0 + (l - r)*pow((x-2*x0)/d,3)/4.0 + (l - r)*(x-2*x0)/d/2.0 + (l + r)/2.0;
     }
 }
 
 double f_x(double x, double l, double r) {
     if (x > x0 - d && x < x0 + d) {
-        return 3.0*(l - r)/4.0/d * (pow((x-x0)/d,2) - 1.0);
+        return 5*(l - r)*pow((x-x0)/d,4)/d/4.0 + (r - l)*pow((x-x0)/d,2)/d + (r + l)/2.0/d;
     } else if (x < d) {
-        return 3.0*(r - l)/4.0/d * (pow(x/d,2) - 1.0);
+        return 5*(r - l)*pow(x/d,4)/d/4.0 + (l - r)*pow(x/d,2)/d + (l + r)/2.0/d;
     } else if (x > 2*x0 - d) {
-        return 3.0*(r - l)/4.0/d * (pow((x-2*x0)/d,2) - 1.0);
+        return 5*(r - l)*pow((x-2*x0)/d,4)/d/4.0 + (l - r)*pow((x-2*x0)/d,2)/d + (l + r)/2.0/d;
     } else {
         return 0.0;
     }
@@ -43,11 +43,11 @@ double f_x(double x, double l, double r) {
 
 double f_xx(double x, double l, double r) {
     if (x > x0 - d && x < x0 + d) {
-        return 3.0*(l - r)/2.0/d/d/d * (x - x0);
+        return 5*(l - r)*pow((x-x0)/d,3)/d/d + 2*(r - l)*(x-x0)/d/d/d;
     } else if (x < d) {
-        return 3.0*(r - l)/2.0/d/d/d * x;
+        return 5*(r - l)*pow(x/d,3)/d/d + 2*(l - r)*x/d/d/d;
     } else if (x > 2*x0 - d) {
-        return 3.0*(r - l)/2.0/d/d/d * (x - 2*x0);
+        return 5*(r - l)*pow((x-2*x0)/d,3)/d/d + 2*(l - r)*(x-2*x0)/d/d/d;
     } else {
         return 0.0;
     }
@@ -55,9 +55,11 @@ double f_xx(double x, double l, double r) {
 
 double f_xxx(double x, double l, double r) {
     if (x > x0 - d && x < x0 + d) {
-        return 3.0*(l - r)/2.0/d/d/d;
-    } else if (x < d && x > 2*x0 - d) {
-        return 3.0*(l - r)/2.0/d/d/d;
+        return 15*(l - r)*pow((x-x0)/d,2)/d/d/d + 2*(r - l)/d/d/d;
+    } else if (x < d) {
+        return 15*(r - l)*pow(x/d,2)/d/d/d + 2*(l - r)/d/d/d;
+    } else if (x > 2*x0 - d) {
+        return 15*(r - l)*pow((x-2*x0)/d,2)/d/d/d + 2*(l - r)/d/d/d;
     } else {
         return 0.0;
     }
@@ -192,7 +194,8 @@ int main(int argc, char **argv) {
 
     double cfl = 0.05;
     double s = 0.0;
-    double a = 1.00;
+    double a = 0.115;
+    double aa = (1.4+1)*a/2;
     double dx = 100.0/NX;
     double dt = cfl*dx;
     int NT = 10/dt;
@@ -208,7 +211,7 @@ int main(int argc, char **argv) {
             printf("it = %d: t = %f\n", navi.time_step, t);
 
             char fn[256];
-            sprintf(fn, "data/%s-%.2f-%.2f-%.3f-%d-%.2f-%f.dat", problem, cfl, s, a, NX, d, t);
+            sprintf(fn, "data/%s-5-%.2f-%.2f-%.3f-%.3f-%d-%.2f-%f.dat", problem, cfl, s, a, aa, NX, d, t);
             FILE *fp = fopen(fn, "w");
 
             for(int ix = navi.lower_x; ix < navi.upper_x; ++ix) {
@@ -221,6 +224,6 @@ int main(int argc, char **argv) {
         Formura_Forward(&navi);
     }
 
-    printf("params: %s-%.2f-%.2f-%.3f-%d-%.2f", problem, cfl, s, a, NX, d);
+    printf("params: %s-5-%.2f-%.2f-%.3f-%.3f-%d-%.2f", problem, cfl, s, a, aa, NX, d);
     MPI_Finalize();
 }
